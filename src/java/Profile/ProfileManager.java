9db3a64f32bcb38b5d56cb769cbfc67aa47c2243
Joe Klinck <jklinck76@gmail.com>
2024-11-26 00:56:24 -0800

    started outline of ProfileManager class

--- src/java/Profile/ProfileManager.java
+++ src/java/Profile/ProfileManager.java
@@ -1,4 +1,31 @@
 package Profile;
 
 public class ProfileManager {
+
+    undirectedGraph friendsList;
+
+    public ProfileManager(){
+
+    }
+
+    // add a profile to the graph, doesn't create a new profile, just adds a profile to the graph
+    public void addProfile(Profile user){
+
+    }
+
+    // call addEdge method from undirectedGraph, this method comes from the
+    // BasicGraphInterface
+    void connectFriends(Profile user1, Profile user2){
+
+    }
+
+    // only needs to display friends names
+    void displayAllProfiles(){
+
+    }
+
+    void removeProfile(Profile user){
+
+    }
+
 }
